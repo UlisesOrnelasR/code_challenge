4b08847bbfb2325f8853accad122d93dc6041d52
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-05-09 23:09:16 -0600

    Linter corrigiendo la legibilidad de mi codigo

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,10 +1,9 @@
-const Reader = require('./../../lib/utils/Reader')
+const Reader = require("../../lib/utils/Reader");
 
-describe('Pruebas para class Reader', () => {
+describe("Pruebas para class Reader", () => {
+    test("Test 1: Probando lectura del archivo", () => {
+        const students = Reader.readJsonFile("./test/data/visualpartnersTest.json");
 
-    test('Test 1: Probando lectura del archivo', () => {
-        const students = Reader.readJsonFile('./test/data/visualpartnersTest.json')
-
-        expect(students.length).toBe(19)
-    })
-})
+        expect(students.length).toBe(19);
+    });
+});

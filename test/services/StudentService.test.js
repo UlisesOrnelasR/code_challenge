d2bdfa6ff952078d3c410ae3f8b9c1a552b1fc21
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-05-10 14:55:50 -0600

    Agregando prueba que falle a StudentService

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -1,7 +1,9 @@
 const StudentService = require('./../../lib/services/StudentService')
 
-describe('', () => {
-    test('', () => {
-        
+describe('Pruebas para StudentService', () => {
+    test('Test 1: Consultando todos los estudiantes', () => {
+        const studets = StudentService.getStudents('./test/data/visualpartnersTest.json')
+
+        expect(studets.length).toBe(12)
     })
 })

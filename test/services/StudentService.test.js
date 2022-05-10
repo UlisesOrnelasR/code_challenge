e5a7f0d59b41d59bc98391f0feb28183e67c40be
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-05-10 15:34:15 -0600

    Agregando prueba que falle al test 2 de StudentService

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -1,3 +1,4 @@
+const Reader = require('../../lib/utils/Reader')
 const StudentService = require('./../../lib/services/StudentService')
 
 describe('Pruebas para StudentService', () => {
@@ -5,5 +6,11 @@ describe('Pruebas para StudentService', () => {
         const studets = StudentService.getStudents('./test/data/visualpartnersTest.json')
 
         expect(studets.length).toBe(19)
+    })
+
+    test('Test 2: Obteniendo los emails de los estudiantes qu etengan certificación', () => {
+        const studentsEmailCertificados = StudentService.getStudentsEmailsCertification(students)
+
+        expect(studentsEmailCertificados.length).toBe(10)
     })
 })

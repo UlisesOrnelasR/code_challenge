cd42944436474c17223142c845182994b9ce012a
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-05-10 17:09:24 -0600

    Agregando Test 1 que falle a StudentController obteniendo los students

---
+++ test/controllers/StudentController.test.js
@@ -0,0 +1,9 @@
+const StudentController = require('./../../lib/controllers/StudentController')
+
+describe('Pruebas para StudentController', () => {
+    test('Test 1: Obteniendo los explores mediante StudentController', () => {
+        const students = StudentController.getStudentsPuente('./test/data/visualpartnersTest.json')
+
+        expect(students.length).toBe(18)
+    })
+})

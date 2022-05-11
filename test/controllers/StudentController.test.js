836bc600809288ca45f9017becdfcda8881a0f18
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-05-10 18:18:40 -0600

    Checkpoint, dandole estilo Airbnb a lo que llevamos

--- test/controllers/StudentController.test.js
+++ test/controllers/StudentController.test.js
@@ -1,23 +1,23 @@
-const StudentController = require('./../../lib/controllers/StudentController')
+const StudentController = require("../../lib/controllers/StudentController");
 
-describe('Pruebas para StudentController', () => {
-    test('Test 1: Obteniendo los explores mediante StudentController', () => {
-        const students = StudentController.getStudentsPuente('./test/data/visualpartnersTest.json')
+describe("Pruebas para StudentController", () => {
+    test("Test 1: Obteniendo los explores mediante StudentController", () => {
+        const students = StudentController.getStudentsPuente("./test/data/visualpartnersTest.json");
 
-        expect(students.length).toBe(19)
-    })
-    test('Test 2: Obteniendo lista de emails de los students con certificacion', () => {
-        const students = StudentController.getStudentsPuente('./test/data/visualpartnersTest.json')
-        const studentsEmails = StudentController.getStudentsEmailsCertificationPuente(students)
+        expect(students.length).toBe(19);
+    });
+    test("Test 2: Obteniendo lista de emails de los students con certificacion", () => {
+        const students = StudentController.getStudentsPuente("./test/data/visualpartnersTest.json");
+        const studentsEmails = StudentController.getStudentsEmailsCertificationPuente(students);
 
-        expect(studentsEmails[0]).toBe('Todd@visualpartnership.xyz')
-        expect(studentsEmails.length).toBe(10)
-    })
-    test('Test 3: Filtrando students con creditos mayores a 500', () => {
-        const students = StudentController.getStudentsPuente('./test/data/visualpartnersTest.json')
-        const studentsConCreditosMayores = StudentController.getStudentsCreditsPuente(students)
+        expect(studentsEmails[0]).toBe("Todd@visualpartnership.xyz");
+        expect(studentsEmails.length).toBe(10);
+    });
+    test("Test 3: Filtrando students con creditos mayores a 500", () => {
+        const students = StudentController.getStudentsPuente("./test/data/visualpartnersTest.json");
+        const studentsConCreditosMayores = StudentController.getStudentsCreditsPuente(students);
 
-        expect(studentsConCreditosMayores[0].credits).toBe(508)
-        expect(studentsConCreditosMayores.length).toBe(11)
-    })
-})
+        expect(studentsConCreditosMayores[0].credits).toBe(508);
+        expect(studentsConCreditosMayores.length).toBe(11);
+    });
+});
--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -1,25 +1,24 @@
+const StudentService = require("../../lib/services/StudentService");
 
-const StudentService = require('./../../lib/services/StudentService')
+describe("Pruebas para StudentService", () => {
+    test("Test 1: Consultando todos los estudiantes", () => {
+        const studets = StudentService.getStudents("./test/data/visualpartnersTest.json");
 
-describe('Pruebas para StudentService', () => {
-    test('Test 1: Consultando todos los estudiantes', () => {
-        const studets = StudentService.getStudents('./test/data/visualpartnersTest.json')
+        expect(studets.length).toBe(19);
+    });
 
-        expect(studets.length).toBe(19)
-    })
+    test("Test 2: Obteniendo los emails de los estudiantes qu etengan certificación", () => {
+        const students = StudentService.getStudents("./test/data/visualpartnersTest.json");
+        const studentsEmailCertificados = StudentService.getStudentsEmailsCertification(students);
 
-    test('Test 2: Obteniendo los emails de los estudiantes qu etengan certificación', () => {
-        const students = StudentService.getStudents('./test/data/visualpartnersTest.json')
-        const studentsEmailCertificados = StudentService.getStudentsEmailsCertification(students)
+        expect(studentsEmailCertificados[0]).toBe("Todd@visualpartnership.xyz");
+        expect(studentsEmailCertificados.length).toBe(10);
+    });
 
-        expect(studentsEmailCertificados[0]).toBe("Todd@visualpartnership.xyz")
-        expect(studentsEmailCertificados.length).toBe(10)
-    })
+    test("Test 3: Obteniendo estudiantes que tengan creditos > 500", () => {
+        const students = StudentService.getStudents("./test/data/visualpartnersTest.json");
+        const studentsCreditosMayores = StudentService.getStudentsCredits(students);
 
-    test('Test 3: Obteniendo estudiantes que tengan creditos > 500', () => {
-        const students = StudentService.getStudents('./test/data/visualpartnersTest.json')
-        const studentsCreditosMayores = StudentService.getStudentsCredits(students)
-
-        expect(studentsCreditosMayores.length).toBe(11)
-    })
-})
+        expect(studentsCreditosMayores.length).toBe(11);
+    });
+});
